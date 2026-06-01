df48b342e67839de0b3f29931f659b7426d8ed36
Christian <christian.deubel@gmail.com>
2026-06-01 11:29:23 +0200

    reduce batch size

--- scripts/run_slurm-gemma4-31b-cot.sh
+++ scripts/run_slurm-gemma4-31b-cot.sh
@@ -13,7 +13,7 @@ REPO=/work/courses/3dv/team29/3D-vision-Benchmarking-Spatial-and-State-Reasoning
 
 MODEL="${MODEL:-gemma4-31b}"
 DATASET="${DATASET:-data/action_phase_dataset.jsonl}"
-BATCH_SIZE="${BATCH_SIZE:-48}"
+BATCH_SIZE="${BATCH_SIZE:-24}"
 
 module load cuda/13.0
 source ~/.bashrc
